c8a1fd3d3532386cf7695c917ae420aa293beb40
helloworld <ura22051995@gmail.com>
2023-02-22 21:55:48 +0300

    Коммит

---
+++ src/main/java/com/example/recipesweb/controllers/FirstController.java
@@ -0,0 +1,26 @@
+package com.example.recipesweb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String helloWeb() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        String name = "Юрий Б.";
+        String project = "Recipes Web";
+        LocalDate data = LocalDate.of(2023, 2, 22);
+        return "автор - " + name +
+                " название проекта - " + project +
+                " дата создания - " + data +
+                " Это первое тестовое веб-приложение для хранения рецептов";
+    }
+}
---
+++ src/test/java/com/example/recipesweb/RecipesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
